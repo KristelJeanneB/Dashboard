f0ef74bf6f13803123a667043dcb60bf68b15753
KristelJeanneB <krid.bautista.up@phinmaed.com>
2023-10-19 10:38:56 +0800

    create goal

--- app/src/main/java/com/example/savingstracker/bottom_fragment/CreategoalFragment.kt
+++ app/src/main/java/com/example/savingstracker/bottom_fragment/CreategoalFragment.kt
@@ -6,8 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.savingstracker.databinding.FragmentCreategoalBinding
+import android.app.DatePickerDialog
+import android.app.Dialog
+import androidx.fragment.app.DialogFragment
+import java.util.Calendar
 
-class CreategoalFragment : Fragment() {
+
+class CreategoalFragment : DialogFragment() {
+
+    private lateinit var datePickerDialog: DatePickerDialog
 
 
     private var _binding: FragmentCreategoalBinding? = null
@@ -26,6 +33,25 @@ class CreategoalFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.btSetDate.setOnClickListener {
+            showDatePicker()
+        }
     }
+
+    private fun showDatePicker() {
+        val cal = Calendar.getInstance()
+        val year = cal.get(Calendar.YEAR)
+        val month = cal.get(Calendar.MONTH)
+        val day = cal.get(Calendar.DAY_OF_MONTH)
+
+        val dateSetListener = DatePickerDialog.OnDateSetListener { _,selectedYear,selectedMonth,selectedDay ->
+            val selectedDate = "$selectedYear-${selectedMonth+1}-$selectedDay"
+            binding.btSetDate.text = selectedDate
+        }
+
+        datePickerDialog = DatePickerDialog(requireContext(),dateSetListener,year,month,day)
+        datePickerDialog.show()
+    }
+
 }
 
--- app/src/main/java/com/example/savingstracker/nav_fragment/HomeFragment.kt
+++ app/src/main/java/com/example/savingstracker/nav_fragment/HomeFragment.kt
@@ -1,10 +1,16 @@
 package com.example.savingstracker.nav_fragment
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.SwitchCompat
 import com.example.savingstracker.R
 import com.example.savingstracker.bottom_fragment.AccountFragment
 import com.example.savingstracker.bottom_fragment.RecommendationsFragment
@@ -13,66 +19,63 @@ import com.example.savingstracker.bottom_fragment.CreategoalFragment
 import com.example.savingstracker.bottom_fragment.WalletFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
+    class HomeFragment : Fragment() {
 
 
-class HomeFragment : Fragment() {
+        override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View? {
+            val view = inflater.inflate(R.layout.fragment_home, container, false)
 
+            val bottomNavigationView =
+                view.findViewById<BottomNavigationView>(R.id.bottomNavigation)
+            bottomNavigationView.setOnItemSelectedListener {
+                when (it.itemId) {
+                    R.id.bottom_category -> {
+                        replaceFragment(CategoryFragment())
+                        activity?.title = "Home"
+                        true
+                    }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_home, container, false)
+                    R.id.bottom_wallet -> {
+                        replaceFragment(WalletFragment())
+                        activity?.title = "Wallet"
+                        true
+                    }
 
-        val bottomNavigationView =
-            view.findViewById<BottomNavigationView>(R.id.bottomNavigation)
-        bottomNavigationView.setOnItemSelectedListener {
-            when (it.itemId) {
-                R.id.bottom_category -> {
-                    replaceFragment(CategoryFragment())
-                    activity?.title = "Home"
-                    true
+                    R.id.bottom_add -> {
+                        replaceFragment(CreategoalFragment())
+                        activity?.title = "Create Goal"
+                        true
+                    }
+
+                    R.id.bottom_chart -> {
+                        replaceFragment(RecommendationsFragment())
+                        activity?.title = "Recommendations"
+                        true
+                    }
+
+                    R.id.bottom_account -> {
+                        replaceFragment(AccountFragment())
+                        activity?.title = "Account"
+                        true
+                    }
                 }
-
-                R.id.bottom_wallet -> {
-                    replaceFragment(WalletFragment())
-                    activity?.title = "Wallet"
-                    true
-                }
-
-                R.id.bottom_add -> {
-                    replaceFragment(CreategoalFragment())
-                    activity?.title = "Create Goal"
-                    true
-                }
-
-                R.id.bottom_chart -> {
-                    replaceFragment(RecommendationsFragment())
-                    activity?.title = "Recommendations"
-                    true
-                }
-
-                R.id.bottom_account -> {
-                    replaceFragment(AccountFragment())
-                    activity?.title = "Account"
-                    true
-                }
+                true
             }
-            true
+            return view
         }
-        return view
+
+
+        private fun startActivity(intent: Unit) {
+
+        }
+
+        private fun replaceFragment(fragment: Fragment) {
+            parentFragmentManager
+                .beginTransaction()
+                .replace(R.id.bottomFragment, fragment)
+                .commit()
+        }
     }
-
-
-    private fun startActivity(intent: Unit) {
-
-    }
-
-    private fun replaceFragment(fragment: Fragment) {
-        parentFragmentManager
-            .beginTransaction()
-            .replace(R.id.bottomFragment, fragment)
-            .commit()
-    }
-
-}
